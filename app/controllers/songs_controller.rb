79a6b0a6ae2eb942cd0ca3f7a7050a5c8db03ae1
Marissa Zhong <mwz2101@columbia.edu>
2018-07-25 14:15:10 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -9,7 +9,7 @@ class SongsController < ApplicationController
 	end
 
 	def create
-	  @song = Song.new(song_params(:'song[name]', :'song[artist_id]', :'song[genre_id]'))
+	  @song = Song.new(song_params)
 	  @song.save
 	  redirect_to song_path(@song)
 	end
@@ -20,11 +20,11 @@ class SongsController < ApplicationController
 
 	def update
 	  @song = Song.find(params[:id])
-		@song.update(song_params(:'song[name]', :'song[artist_id]', :'song[genre_id]'))
+		@song.update(song_params)
     redirect_to song_path(@song)
 	end
 
 	def song_params(*args)
-		params.require(:song).permit(*args)
+		params.require(:song).permit(:name, :artist_id, :genre_id)
 	end
 end
