52d4c3d936309b5ddb23bd1dc8e075f8f1e89200
Marissa Zhong <mwz2101@columbia.edu>
2018-07-25 13:48:07 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -24,7 +24,7 @@ class SongsController < ApplicationController
     redirect_to song_path(@song)
 	end
 
-	def Song_params(*args)
-		params.require(:Song).permit(*args)
+	def song_params(*args)
+		params.require(:song).permit(*args)
 	end
 end
